3a404c5781961028f180071709c8b3191f9dfb43
MateusChavito <mateus.chavito@hotmail.com>
2024-06-15 13:57:25 -0300

    Tarefa da ebac para realizar média das notas dos alunos mas com resultado de aprovação

---
+++ untitled/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args){
+            int nota1 = 6;
+            int nota2 = 8;
+            int nota3 = 8;
+            int nota4 = 3;
+            float result =(nota1+nota2+nota3+nota4)/4;
+                System.out.println("O resultado é:"+ result);
+
+
+            if (result >= 7){
+                System.out.println("Aprovado");
+            } else if (result >= 5){
+                System.out.println("Recuperação");
+            } else{
+            System.out.println("Reprovado");
+            }
+
+            //Poderia me responder na correção, o uso do float deveria colocar
+            // o f no final do result ?
+
+        }
+
+
+
+    }
